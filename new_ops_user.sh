fb2c43dea910e05035eff44759fa531b356675db
Andy Gallegos <andysgallegos@gmail.com>
2022-10-12 19:16:03 -0400

    Update new_ops_user.sh

--- new_ops_user.sh
+++ new_ops_user.sh
@@ -1,36 +1,35 @@
-#Creates a new operations user with access to the /home/files/ directory.
-#user will have no login shell, random password, home directory, and be member of sftpusers group
-#run script followed by username
-#example: sudo ./create_ops_user.sh testuser
+#!/bin/bash
 
-
-FSTAB="bindfs#/home/files /home/users/$USER/files fuse force-user=$USER,force-group=$USER,create-for-user=$USER,create-for-group=sftpusers,create-with-perms=0750,chgrp-ignore,chown-ignore,chmod-ignore 1 2"
-USER=$1
+#set variables
 PASSWORD=$(pwgen -Bcn 10 1)
-
+USER=$1
 if [ -z "$1" ]; then
     echo "No username given"
     exit 1
 fi
 
-#create user with no shell access, add them to sftpusers group, and make home directory
-useradd -m -d /home/users/$USER  -G sftpusers --shell=/bin/false $USER
+#Add user to  /etc/vsftpd/virtual-users-be.txt
+echo "$USER" >> /etc/test.txt
+echo "$PASSWORD" >> /etc/test.txt
+#sudo /etc/vsftpd/make
 
-#make files and set correct permissions so they only see their directory after login
-mkdir /home/users/$USER/files
-chown root:$USER /home/users/$USER/
-chown $USER:$USER /home/users/$USER/files
-chmod 755 /home/users/$USER
+#Make users home directory and set ownership to vds
+mkdir /home/ftp/$USER
+chown vds.vds /home/ftp/$USER
+#service vsftpd restart
 
+#if the user is a operations user, bind mount them to the home/ftp directory.
+FSTAB="/home/ftp /home/ftp/$USER  none   bind   0 0"
+#echo "Is this person an BEP Employee?"
+#read ANSWER
+#if [ "$ANSWER" == 'y' ]; then
+#   echo $FSTAB  | sudo tee -a /etc/fstab
+#fi
 
-#update FSTAB to bind the user to /home/files, mount the bind, and then comment out the most recent line
-echo "$FSTAB" | sudo tee -a /etc/fstab  1>/dev/null
-LINE=$(sudo cat /etc/fstab | wc -l)
-mount -a
-sudo sed -i  "$LINE s/^/#/" /etc/fstab
-
-#output the folllowing details to terminal
-echo "$USER:$PASSWORD" | chpasswd
-echo "Account created"
+read -p "Is this user a BEP empoyee? (y/n) " yn
+ case $yn in
+     [yY] ) echo $FSTAB | sudo tee -a /etc/fstab  1>/dev/null && mount  -a;;
+     [nN] ) echo  "User has been created, see details below";;
+ esac
 echo "Username: $USER"
 echo "Password: $PASSWORD"
